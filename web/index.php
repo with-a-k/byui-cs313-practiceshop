a121252c361ab80dbdd2c8a70428a09c1a2f0529
Erik Butcher <with_a_k@mac.com>
2020-10-02 15:38:17 -0600

    Make PHP if{} an if:...endif;

--- web/index.php
+++ web/index.php
@@ -15,13 +15,13 @@
             <p><?=$item->get_desc()?></p>
             <div class="inputs">
               <p><?=$item->get_cost()?></p>
-              <?php if($item->get_options()) { ?>
+              <?php if($item->get_options()): ?>
                 <select name='<?php echo $item->get_name()?>-color'>
                   <?php foreach(count($item->get_options()) > 0 as color) { ?>
                     <option value='<?php echo color?>'><?=color?></option>
                   <?php } ?>
                 </select>
-              <?php } ?>
+              <?php endif; ?>
             </div>
           </div>
         </li>
