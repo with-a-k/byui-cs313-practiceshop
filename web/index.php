8205a5011789ef765c6e9b01f32664a3761ce905
Erik Butcher <with_a_k@mac.com>
2020-10-02 15:37:19 -0600

    Add color select

--- web/index.php
+++ web/index.php
@@ -8,18 +8,24 @@
     <?php include 'items.php'?>
 
     <ul>
-      <?php foreach ($items as $item) { ?>
+      <?php foreach ($items as $item): ?>
         <li>
           <div class="item">
             <h3><?=$item->get_name()?></h3>
             <p><?=$item->get_desc()?></p>
             <div class="inputs">
               <p><?=$item->get_cost()?></p>
-              
+              <?php if($item->get_options()) { ?>
+                <select name='<?php echo $item->get_name()?>-color'>
+                  <?php foreach(count($item->get_options()) > 0 as color) { ?>
+                    <option value='<?php echo color?>'><?=color?></option>
+                  <?php } ?>
+                </select>
+              <?php } ?>
             </div>
           </div>
         </li>
-      <?php } ?>
+      <?php endforeach; ?>
     </ul>
     <a href="cart.php">View Cart</a>
   </body>
