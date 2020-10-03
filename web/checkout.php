f08e7e75903bde9fc19119e5def15aab4103ca75
Erik Butcher <with_a_k@mac.com>
2020-10-03 09:43:11 -0600

    Add missing City field to address form

--- web/checkout.php
+++ web/checkout.php
@@ -11,6 +11,7 @@
 
     <form name="address" action="confirm.php" method="POST">
       Street Address: <input type="text" name="address-street">
+      City: <input type="text" name="address-city">
       State/Province: <input type="text" name="address-state">
       Country: <input type="text" name="address-country">
       Postal Code: <input type="text" name="address-postcode">
--- web/confirm.php
+++ web/confirm.php
@@ -1,7 +1,11 @@
 <?php
   include 'items.php';
   session_start();
-  var_dump($_POST);
+  $address_street = htmlspecialchars($_POST['address-street']);
+  $address_city = htmlspecialchars($_POST['address-city']);
+  $address_state = htmlspecialchars($_POST['address-state']);
+  $address_country = htmlspecialchars($_POST['address-country']);
+  $address_postcode = htmlspecialchars($_POST['address-postcode']);
 ?>
 <html>
   <head>
@@ -9,6 +13,12 @@
     <link rel=stylesheet href="styles/foundation.css">
   </head>
   <body>
-
+    <div class="confirm">
+      <p>Thank you for your purchase! Your items totaled $<?=$_SESSION['totalCost']?>.</p>
+      <p>They will be sent to the address you submitted:</p>
+      <p><?php echo $address_street ?></p>
+      <p><?php echo $address_city . ", " . $address_state . ", " .
+      $address_country . " " . $address_postcode ?></p>
+    </div>
   </body>
 </html>
