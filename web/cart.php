4fd3f52eb921ccf026d483c572b7cecab72c74af
Erik Butcher <with_a_k@mac.com>
2020-10-02 19:50:16 -0600

    Add cart page

--- web/cart.php
+++ web/cart.php
@@ -1,11 +1,39 @@
-<?php session_start() ?>
+<?php
+  session_start();
+  include 'items.php';
+  foreach ($_SESSION['cart'] as $index => $item) {
+    if (isset($_POST["item-".$index])) {
+      unset($_SESSION['cart'], $index);
+    }
+  }
+?>
 <html>
   <head>
     <title>Shop</title>
     <link rel=stylesheet href="styles/foundation.css">
   </head>
   <body>
-    <?php include 'items.php'?>
+    <h2>Your Cart</h2>
+    <ul>
+      <?php foreach ($_SESSION['cart'] as $index => $item): ?>
+        <?php $form_name = "item-".$index ?>
+        <li>
+          <div class="item">
+            <h3><?=$item->get_name()?></h3>
+            <p><?=$item->get_desc()?></p>
+            <div class="inputs">
+              <p><?=$item->get_cost()?></p>
+              <?php if(strlen($item->get_color()) > 0): ?>
+                <p><?=$item->get_color()?></p>
+              <?php endif; ?>
+              <form id='<?php echo $form_name ?>' method="POST" action="index.php">
+                <input type="submit" value="Remove from cart">
+              </form>
+            </div>
+          </div>
+        </li>
+      <?php endforeach; ?>
+    </ul>
 
   </body>
 </html>
